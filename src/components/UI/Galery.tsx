629effa4bd0cdc9b9bcdf41a025922a593432748
kkgrz13 <k.kozlowski684@gmail.com>
2025-06-12 19:07:49 +0200

    UPDATE: Working on galery

--- src/components/UI/Galery.tsx
+++ src/components/UI/Galery.tsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 export default function Galery() {
   const galeryContent = ["0", "1", "2"];
   let selectedContent = 1;
+  const [galery, setGalery] = useState(galeryContent[selectedContent]);
 
   const buttonLeft = (
     <button
       className="z-1 absolute left-0 top-0 w-15 h-full flex items-center opacity-20 hover:opacity-50 hover:scale-125 transition duration-50"
       onClick={(e) => {
-        e.preventDefault();
+        // e.preventDefault();
         if (selectedContent == 0) {
           selectedContent = 2;
+          setGalery(galeryContent[selectedContent]);
+          console.log(galery);
         } else {
           selectedContent -= 1;
+          setGalery(galeryContent[selectedContent]);
+          console.log(galery);
         }
-        console.log(selectedContent);
       }}
     >
       <div className="z-1 w-15 h-15 rotate-135 inset-shadow-arrow"></div>
@@ -29,7 +35,8 @@ export default function Galery() {
         } else {
           selectedContent += 1;
         }
-        console.log(selectedContent);
+        console.log(galery);
+        setGalery(galeryContent[selectedContent]);
       }}
     >
       <div className="z-1 w-15 h-15 rotate-315 inset-shadow-arrow"></div>
@@ -37,9 +44,9 @@ export default function Galery() {
   );
 
   return (
-    <div className="relative">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
-        <div className="z-1 absolute w-full h-full flex flex-col justify-center pl-150 pr-30 overflow-hidden bg-gradient-to-r from-lazuli-t to-lazuli-900 to-40% text-lazuli-200">
+    <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+      <div className="overflow-hidden">
+        <div className="z-1 absolute w-full h-full flex flex-col justify-center pl-150 pr-30 bg-gradient-to-r from-lazuli-t to-lazuli-900 to-40% text-lazuli-200">
           <h3 className="text-5xl font-bold mb-5">Nazwa projektu</h3>
           <p className="text-xl">
             Opis projektu Lorem ipsum dolor sit amet, consectetur adipiscing
@@ -49,8 +56,8 @@ export default function Galery() {
           </p>
         </div>
         <img
-          className="h-full blur-xs"
-          src={`src\\assets\\img\\Project_${galeryContent[selectedContent]}.png`}
+          className="size-full blur-xs"
+          src={`src\\assets\\img\\Project_${galery}.png`}
           alt="Project Photo"
         />
       </div>
