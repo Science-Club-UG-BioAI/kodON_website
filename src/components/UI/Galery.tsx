358ea35040507628ef6b23f3458eadb1a02fb4be
kkgrz13 <k.kozlowski684@gmail.com>
2025-06-14 12:48:57 +0200

    UPDATE: Gelery Galery is done, for now there are placeholder images and descritions. For now its nto responsive on moblile

--- src/components/UI/Galery.tsx
+++ src/components/UI/Galery.tsx
@@ -28,28 +28,38 @@ export default function Galery() {
       <div className="relative size-full overflow-hidden">
         <div className="z-1 absolute size-full grid grid-cols-11 grid-rows-11 bg-gradient-to-r from-lazuli-t to-lazuli-900 to-40% text-lazuli-200">
           <button
-            className="z-2 flex justify-center items-center col-span-2 col-start-1 row-span-11 row-start-1 opacity-20 hover:opacity-50 hover:scale-125 transition duration-50"
+            className="z-2 cursor-pointer flex justify-center items-center col-span-2 col-start-1 row-span-11 row-start-1 opacity-20 transition duration-100 hover:opacity-50 hover:scale-125 transition duration-50"
             onClick={handleLeftClick}
           >
             <div className="w-15 h-15 rotate-135 inset-shadow-arrow"></div>
           </button>
-
           <button
-            className="z-2 flex justify-center items-center col-span-2 col-start-10 row-span-11 row-start-1 opacity-20 hover:opacity-50 hover:scale-125 transition duration-50"
+            className="z-2 cursor-pointer flex justify-center items-center col-span-2 col-start-10 row-span-11 row-start-1 opacity-20 transition duration-100 hover:opacity-50 hover:scale-125 transition duration-50"
             onClick={handleRightClick}
           >
             <div className="w-15 h-15 rotate-315 inset-shadow-arrow"></div>
           </button>
-
           <h3 className="col-span-4 col-start-6 row-span-1 row-start-4 text-5xl font-bold mb-5">
             {projectName[selectedContent]}
           </h3>
-          <p className="col-span-4 col-start-6 row-span-5 row-start-5 text-xl">
+          <p className="col-span-4 col-start-6 row-span-5 row-start-5 text-xl ">
             {porjectDesc[selectedContent]}
           </p>
+          <div className="col-span-3 col-start-5 row-start-10 flex items-center justify-between px-20">
+            {[0, 1, 2].map((i) => (
+              <div
+                key={i}
+                className={`bg-lazuli-200 opacity-30 size-10 rounded-full transition flex items-center justify-center after:content-[''] after:block after:size-6 after:bg-lazuli-400 after:rounded-full after:opacity-30 duration-100 transform ${
+                  selectedContent === i
+                    ? "opacity-80 scale-110 shadow-md after:bg-lazuli-600 after:opacity-80"
+                    : ""
+                }`}
+              />
+            ))}
+          </div>
         </div>
         <img
-          className="size-full aspect-3/2 blur-xs"
+          className="size-full aspect-3/2 blur-xs object-cover transition-opacity duration-500"
           src={`src/assets/img/Project_${galeryContent[selectedContent]}.png`}
           alt="Project Photo"
         />
