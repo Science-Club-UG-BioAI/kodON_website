27299d3b563ca38b8fa796a4ef3085b4c04738f5
Natalia Zagórska <vermenea@gmail.com>
2025-06-16 15:26:45 +0200

    feat: add footer component with social links and image gallery

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 
@@ -7,6 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Header />
       <Main />
       {children}
+      <Footer/>
     </div>
   );
 }
--- src/components/UI/LogoFooter.tsx
+++ src/components/UI/LogoFooter.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+import blacklogo from '/blacklogo.svg';
+
 export default function LogoFooter() {
-  return <div>B&W logo kontener</div>;
+  return (
+     <Link to='/'>
+      <img src={blacklogo} alt='kodON logo' className='h-24 w-auto md:h-28 lg:h-28'/>
+    </Link>
+  )
 }
