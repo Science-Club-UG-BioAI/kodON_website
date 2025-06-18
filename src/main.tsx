e584c58ac09918b0820e940eabdbf4442f60dccf
vermenea <vermenea@gmail.com>
2025-06-18 16:36:18 +0200

    totally normal commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./style/index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <StrictMode className="bg-gradient-to-r from-lazuli-900 to-lazuli-800 !important">
     <App />
   </StrictMode>
 );
