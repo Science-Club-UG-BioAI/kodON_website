efc5714b20e912d5221e445e6c57b6997ecb3bcd
kkgrz13 <k.kozlowski684@gmail.com>
2025-06-18 21:38:47 +0200

    UPDATE: galery resposive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,26 +1,6 @@
-import { useState, useEffect } from "react";
-
 export default function About() {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => setScrollY(window.scrollY);
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="relative w-full bg-lazuli-400 overflow-hidden">
-      {/* Flare effect */}
-      <div
-        className="absolute z-0 w-[3000px] h-[3000px] rounded-full bg-radial from-lazuli-400 to-white from-40% opacity-15  pointer-events-none"
-        style={{
-          top: `${scrollY * 0.1 - 1000}px`,
-          left: `${Math.sin(scrollY / 300) * 100 + 1000}px`,
-          transition: "top 0.1s ease-out, left 0.1s ease-out",
-        }}
-      ></div>
-
       {/* Content */}
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
         <h2 className="font-bold text-4xl px-4 bg-gradient-to-r from-lazuli-900 to-lazuli-600 text-transparent bg-clip-text inline-block">
--- src/components/UI/Galery.tsx
+++ src/components/UI/Galery.tsx
@@ -31,25 +31,27 @@ export default function Galery() {
             className="z-2 cursor-pointer flex justify-center items-center col-span-2 col-start-1 row-span-11 row-start-1 opacity-20 transition duration-100 hover:opacity-50 hover:scale-125 "
             onClick={handleLeftClick}
           >
-            <div className="w-15 h-15 rotate-135 inset-shadow-arrow"></div>
+            <div className="aspect-square w-[4.5vw]  rotate-135 inset-shadow-arrow"></div>
           </button>
           <button
             className="z-2 cursor-pointer flex justify-center items-center col-span-2 col-start-10 row-span-11 row-start-1 opacity-20 transition duration-100 hover:opacity-50 hover:scale-125"
             onClick={handleRightClick}
           >
-            <div className="w-15 h-15 rotate-315 inset-shadow-arrow"></div>
+            <div className="aspect-square w-[4.5vw] rotate-315 inset-shadow-arrow"></div>
           </button>
-          <h3 className="col-span-4 col-start-6 row-span-1 row-start-4 text-5xl font-bold mb-5">
+          <h3 className="col-span-4 col-start-6 row-span-1 row-start-4 text-[2vw] font-bold mb-5">
             {projectName[selectedContent]}
           </h3>
-          <p className="col-span-4 col-start-6 row-span-5 row-start-5 text-xl ">
+          <p className="col-span-4 col-start-6 row-span-5 row-start-5 text-[1vw]">
             {porjectDesc[selectedContent]}
           </p>
-          <div className="col-span-3 col-start-5 row-start-10 flex items-center justify-between px-20">
+
+          {/* Dots */}
+          <div className="col-span-3 col-start-5 row-start-10 flex items-center justify-between px-[27%]">
             {[0, 1, 2].map((i) => (
               <div
                 key={i}
-                className={`bg-lazuli-200 opacity-30 size-10 rounded-full transition flex items-center justify-center after:content-[''] after:block after:size-6 after:bg-lazuli-400 after:rounded-full after:opacity-30 duration-100 transform ${
+                className={`after:content-[''] aspect-square after:aspect-square h-[55%] after:h-[60%] rounded-full after:rounded-full flex after:block items-center justify-center transition bg-lazuli-200 after:bg-lazuli-400 opacity-30 after:opacity-30 duration-1 transform ${
                   selectedContent === i
                     ? "opacity-80 scale-110 shadow-md after:bg-lazuli-800 after:opacity-80"
                     : ""
