48eb29ece52c9511f2bfb02f1ba04c8ee85e7767
Natalia Zagórska <vermenea@gmail.com>
2025-06-19 20:48:49 +0200

    fix: white bar on top & bottom on mobile fixed - added blackish color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
 
 function App() {
-  return (
-    <div className="min-h-[100dvh] bg-gradient-to-r from-lazuli-900 to-lazuli-800">
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
