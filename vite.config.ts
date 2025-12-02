414714e20eaa6713b1e1a5745852c6156cfb5381
kkgrz13 <k.kozlowski684@gmail.com>
2025-12-02 23:09:36 +0100

    ADD: zmianyt pod hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: '/kodON_website/',
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  base: "/kodON_website/",
+  plugins: [tailwindcss()],
+});
